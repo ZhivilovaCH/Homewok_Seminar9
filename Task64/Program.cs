33b1f4f8ae3914546836373b7c58d562c507bcc9
Christina Zhivilova <krishakirova@yandex.ru>
2022-11-18 15:46:30 +0300

    Добавление задачи

---
+++ Task64/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+
+void GetN(int n)
+{
+
+    while (n != 0)
+    {
+        Console.Write($"{n}, ");
+        n -= 1;
+
+    }
+}
+
+int GetN(int n)
+{
+    if (n == 0)
+    {
+        return n;   
+    }
+    else if(n>0){
+    Console.Write($"{n}, ");
+    return GetN(n-1);
+    }
+    else return n;
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+
+GetN(n);
+
+
+
+
---
+++ Task66/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+int GetNumbers(int m, int n)
+{
+    int number = 0;
+
+    for (int i = m; i <= n; i++)
+    {
+        number += i;
+    }
+    return number;
+}
+
+
+void PrintNumbers(int number)
+{
+    Console.WriteLine(number);
+}
+
+
+Console.Clear();
+Console.Write("Введите первое число, в промежутке от : ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число, до числа : ");
+int n = int.Parse(Console.ReadLine()!);
+
+int number = GetNumbers(m, n);
+Console.Write($"Cумму натуральных элементов в промежутке от {m} до {n} = ");
+PrintNumbers(number);
